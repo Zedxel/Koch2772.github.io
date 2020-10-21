e1aa014ed6e17b84f68e09c8b4ae1eba135f646f
Jaredgib <gibs0036@flinders.edu.au>
2020-10-21 17:37:46 +1030

    Updated database / FINAL 1.0

--- database/MCP_Bld.sql
+++ database/MCP_Bld.sql
@@ -6,7 +6,7 @@ CREATE DATABASE MCP;
 USE MCP;
 
 CREATE TABLE Customer(
-	custId id AUTO_INCREMENT PRIMARY KEY,
+	custId INT AUTO_INCREMENT,
 	title varchar(8),
 	fname varchar(40) NOT NULL,
 	lname varchar(40) NOT NULL,
@@ -59,14 +59,14 @@ CREATE TABLE Product (
 );
 
 CREATE TABLE Card (
-	custId varchar(8) NOT NULL,
+	custId INT NOT NULL,
 	cardNum	char(21) NOT NULL,
 	PRIMARY KEY(custId),
 	FOREIGN KEY(custId) REFERENCES Customer(custId)
 );
 
 CREATE TABLE Cart (
-	custId varchar(8) NOT NULL,
+	custId INT NOT NULL,
 	stockNum varchar(10),
 	unitCost decimal(6,2),
 	qty int(3),
@@ -75,20 +75,20 @@ CREATE TABLE Cart (
 );
 
 CREATE TABLE Invoice (
-	custId varchar(8)NOT NULL PRIMARY KEY,
-	invoiceId AUTO_INCREMENT PRIMARY KEY,
+	invoiceId INT AUTO_INCREMENT,
+	custId INT NOT NULL,
 	invoiceDate	timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	address varchar(45) NOT NULL,
 	address2 varchar(45),
 	apartType varchar(45),
 	Suburb varchar(45) NOT NULL,
 	galaxy varchar(45) NOT NULL,
-	system varchar(45) NOT NULL,
+	sSystem varchar(45) NOT NULL,
 	Postcode char(5) NOT NULL,
 	phone varchar(14) NOT NULL DEFAULT 555,
 	deliveryFee decimal(5,2),
 	finCost decimal(8,2) NOT NULL,
-	PRIMARY KEY (custId, invoiceId),
+	PRIMARY KEY (invoiceId, custId),
 	FOREIGN KEY(custId) REFERENCES Customer(custId)
 );
 
--- database/MCP_data.sql
+++ database/MCP_data.sql
@@ -1,46 +1,4 @@
-INSERT INTO Customer VALUES ('0001', 'Mr', 'Warren', 'Peece');
-INSERT INTO Customer VALUES ('0002', 'Mr', 'Hugo', 'First');
-INSERT INTO Customer VALUES ('0003', 'Mr', 'Juan', 'Anatu');
-INSERT INTO Customer VALUES ('0004', 'Miss', 'Lauren', 'Order');
-INSERT INTO Customer VALUES ('0005', 'Mrs', 'Fay', 'Tality');
-INSERT INTO Customer VALUES ('0006', 'Mrs', 'Theresa', 'Crowd');
-INSERT INTO Customer VALUES ('0007', 'Ms', 'Jean', 'Poole');
-INSERT INTO Customer VALUES ('0008', 'Mr', 'Walter', 'Wall');
-INSERT INTO Customer VALUES ('0009', 'Mr', 'Dale', 'Neverno');
-INSERT INTO Customer VALUES ('0010', 'Miss', 'Lotta', 'Noyes');
-
-INSERT INTO Details VALUES ('0001', 'peeceW', 'j3t747');
-INSERT INTO Details VALUES ('0002', 'firsth', 'j3llyf1sh');
-INSERT INTO Details VALUES ('0003', 'anatuj', 'h0tc0f33');
-INSERT INTO Details VALUES ('0004', 'orderl', 'b00kcas3');
-INSERT INTO Details VALUES ('0005', 'talityf', 'd3adrit3');
-INSERT INTO Details VALUES ('0006', 'crowdt', 'n0m0re');
-INSERT INTO Details VALUES ('0007', 'poolej', 'chl0r1ne');
-INSERT INTO Details VALUES ('0008', 'wallw', 'l1n0leum');
-INSERT INTO Details VALUES ('0009', 'nevernod', '1gn0rant');
-INSERT INTO Details VALUES ('0010', 'noyesl', 'drumk1t');
-
-INSERT INTO CustResAddr VALUES ('0001', '34 Yellow Brick Road', 'Asbury Park', '21047', '03548 621 6711', 'wp67@home.today');
-INSERT INTO CustResAddr VALUES ('0002', '3 Abbey Road', 'Mansfield', '19045', '84566 004 7470', 'jpgr4@lunch.right.now');
-INSERT INTO CustResAddr VALUES ('0003', '67 Copperhead Road', 'Strawberry Fields', '04015', '06444 202 1902', 'iam@work.need.cash');
-INSERT INTO CustResAddr VALUES ('0004', '231 Thunder Road', 'Backwater', '14117', '03656 333 0101', 'fee@car.for.sale');
-INSERT INTO CustResAddr VALUES ('0005', 'Lot 351 Telegraph Road', 'Ghosttown', '62357', '09852 007 1852', 'stay@hotel.for.work');
-INSERT INTO CustResAddr VALUES ('0006', 'U4, 56 Longnwinding Road', 'Arboria', '23995', '04512 707 1066', 'crowd@the.concert');
-INSERT INTO CustResAddr VALUES ('0007', '529 Country Road', 'Kokomo', '33827', '01202 999 1972', 'jp007@thepub.quiz');
-INSERT INTO CustResAddr VALUES ('0008', '26 52nd Street', 'Suffragette City', '45674', '02987 351 1888', 'wally@thepark.hiding');
-INSERT INTO CustResAddr VALUES ('0009', '461 Ocean Boulevard', 'Blueberry Hill', '19934', '06951 665 6655', 'neverno@the.bigsmoke');
-INSERT INTO CustResAddr VALUES ('0010', '761A Gasoline Alley', 'Jurassic Park', '16637', '09511 000 2512', 'noysey@the.midnite.hour');
-
-INSERT INTO CustDelAddr VALUES ('0001', '34 Yellow Brick Road', 'Asbury Park', '21047', ""); 
-INSERT INTO CustDelAddr VALUES ('0002', '3 Abbey Road', 'Mansfield', '19045', 'Stop putting it in the letterbox, it doesnt fit');
-INSERT INTO CustDelAddr VALUES ('0003', 'U7 86 Sterling Grove', 'Steepe Hills', '65957', 'Road floods in the winter - take a pair of wellies');
-INSERT INTO CustDelAddr VALUES ('0004', '3266 Panorama Circuit', 'China Grove', '78219', 'Please leave parcel with sister at number 12');
-INSERT INTO CustDelAddr VALUES ('0005', 'Lot 351 Telegraph Road', 'Ghosttown', '62357', 'Don\'t ring the bell, you\'ll annoy the dog');
-INSERT INTO CustDelAddr VALUES ('0006', 'U4, 56 Longnwinding Road', 'Arboria', '23995', 'Tree Men; do not send Gordon to deliver');
-INSERT INTO CustDelAddr VALUES ('0007', '529 Country Road', 'Kokomo', '33827', "");
-INSERT INTO CustDelAddr VALUES ('0008', '26 52nd Street', 'Suffragette City', '45674', "");
-INSERT INTO CustDelAddr VALUES ('0009', 'Level 82 The Squeegee', 'Hillen Dale', '24006', 'The middle lift only goes to floor 20');
-INSERT INTO CustDelAddr VALUES ('0010', '761A Gasoline Alley', 'Jurassic Park', '16637', "");
+INSERT INTO Customer VALUES ('1', 'Mr', 'Warren', 'Peece', '8350 8350', 'test@test.com', 'test', 'test');
 
 INSERT INTO Product VALUES ('drag6510', 'Dragon scale', 'Bronze dragon scale - medium size', 65.35, 266, 'Scale'); 
 INSERT INTO Product VALUES ('unic1100', 'Unicorn horn', '15" long, no cracks', 480.00, 8, 'Horn');
@@ -55,35 +13,9 @@ INSERT INTO Product VALUES ('merm3500', 'Mermaid scale', 'Different colors, kept
 INSERT INTO Product VALUES ('phnx7350', 'Phoenix beak', 'Upper and lower beaks, some scorch marks', 1200.00, 6, 'Beak');
 INSERT INTO Product VALUES ('basi9550', 'Basilisk fang', 'Large fangs, venom removed, clean', 100.00, 580, 'Fangs');
 
-INSERT INTO Card VALUES ('0001', '0005 21568 78562 0011');
-INSERT INTO Card VALUES ('0002', '0005 55684 32100 1100');
-INSERT INTO Card VALUES ('0003', '0005 88844 36963 0110');
-INSERT INTO Card VALUES ('0004', '0005 15937 46820 1010');
-INSERT INTO Card VALUES ('0005', '0110 12389 98721 0100');
-INSERT INTO Card VALUES ('0006', '0110 64951 22881 0010');
-INSERT INTO Card VALUES ('0007', '0105 36415 85258 0001');
-INSERT INTO Card VALUES ('0008', '0105 21181 34729 1001');
-INSERT INTO Card VALUES ('0009', '0105 13815 45229 1000');
-INSERT INTO Card VALUES ('0010', '0105 67990 18723 0101');
+INSERT INTO Card VALUES ('1', '0005 21568 78562 0011');
 
-INSERT INTO Cart VALUES ('0008', 'drag6510', 85.35, 18, 1536.30);
-INSERT INTO Cart VALUES ('0008', 'cent8020', 600.00, 2, 1200.00);
-INSERT INTO Cart VALUES ('0010', 'drag6510', 85.35, 30, 2560.50);
-INSERT INTO Cart VALUES ('0002', 'unic1100', 480.00, 1, 480.00);
-INSERT INTO Cart VALUES ('0006', 'wyvn4490', 240.50, 2, 481.00);
-INSERT INTO Cart VALUES ('0006', 'merm3500', 10.00, 20, 200.00);
-INSERT INTO Cart VALUES ('0006', 'grif2340', 210.00, 4, 840.00);
-INSERT INTO Cart VALUES ('0005', 'grif2350', 630.30, 1, 630.30);
-INSERT INTO Cart VALUES ('0005', 'wyvn4490', 240.50, 2, 481.00);
-INSERT INTO Cart VALUES ('0005', 'cent8020', 600.00, 4, 2400.00);
-INSERT INTO Cart VALUES ('0003', 'phnx7350', 1200.00, 1, 1200.00);
-INSERT INTO Cart VALUES ('0010', 'basi9550', 100.00, 4, 400.00);
-INSERT INTO Cart VALUES ('0010', 'drag6550', 297.60, 2, 595.20 );
+INSERT INTO Cart VALUES ('1', 'drag6510', 85.35, 18, 1536.30);
+INSERT INTO Cart VALUES ('1', 'cent8020', 600.00, 2, 1200.00);
 
-INSERT INTO Invoice VALUES ('MCP00556', '0008', '2010-10-12', 20.00, 2756.30);
-INSERT INTO Invoice VALUES ('MCP00599', '0010', '2010-10-12', 20.00, 2580.00);
-INSERT INTO Invoice VALUES ('MCP00610', '0002', '2010-10-13', 20.00, 480.00);
-INSERT INTO Invoice VALUES ('MCP00612', '0006', '2010-11-08', 20.00, 1541.00);
-INSERT INTO Invoice VALUES ('MCP00613', '0005', '2010-11-21', 20.00, 3531.30);
-INSERT INTO Invoice VALUES ('MCP00614', '0003', '2010-12-15', 20.00, 1220.00);
-INSERT INTO Invoice VALUES ('MCP00616', '0010', '2011-01-06', 20.00, 1015.00);
+
