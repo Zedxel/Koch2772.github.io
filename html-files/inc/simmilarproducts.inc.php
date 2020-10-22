4a668e1d17b32d5fd597c092e318fe07e806b878
Zedxel <Thomas.Temby@hotmail.com>
2020-10-22 20:52:54 +1030

    Fixed it so that it opens a new db on its own

--- html-files/inc/simmilarproducts.inc.php
+++ html-files/inc/simmilarproducts.inc.php
@@ -3,12 +3,19 @@
     </div>
     <div class="main-page-content">
         <div class="categories">
-            <div class="catagory-container">
-                <ul class="catagory-list">
+            <div class="category-container">
+                <ul class="category-list">
                 <?php
+                    
                     require_once "inc/dbconn.inc.php";
+                    define("DB_HOST", "localhost");
+                    define("DB_NAME", "mcp");
+                    define("DB_USER", "dbadmin");
+                    define("DB_PASS", "");
+
+                    $conn = @mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
                     $sql = "SELECT * FROM `product` WHERE `stockNum` LIKE '";
-                    $sql .= $stockNum;
+                    $sql .= $StockNum;
                     $sql .= "' ORDER BY `category` ASC";
                     if($result = mysqli_query($conn, $sql)){
                         if(mysqli_num_rows($result) > 0){
@@ -28,8 +35,8 @@
                         if(mysqli_num_rows($result) > 0){
                             $u=0;
                             while ($row = mysqli_fetch_assoc($result)) {
-                                if($row["stockNum"] != $stockNum){
-                                    echo    "<div class=\"catagory-product-1x4\">";
+                                if($row["stockNum"] != $StockNum){
+                                    echo    "<div class=\"category-product-1x4\">";
                                     echo        "<a href=\"#\">";
                                     echo            "<img src=\"productimages/";
                                     echo            $tmp = $row["stockNum"];
@@ -52,14 +59,16 @@
                     
                     
                     $sql = "SELECT * FROM `product` WHERE `stockNum` LIKE '%";
-                    $sql .= substr($stockNum,0,4);
+                    $sql .= substr($StockNum,0,4);
                     $sql .= "%'";
                     if($result = mysqli_query($conn, $sql)){
                         if(mysqli_num_rows($result) > 0){
                             while ($row = mysqli_fetch_assoc($result)) {
-                                if($row["stockNum"] != $stockNum){
-                                    echo    "<div class=\"catagory-product-1x4\">";
-                                    echo        "<a href=\"#\">";
+                                if($row["stockNum"] != $StockNum){
+                                    echo    "<div class=\"category-product-1x4\">";
+                                    echo        "<a href=\"product-";
+                                    echo        $row["stockNum"];
+                                    echo        ".php\">";
                                     echo            "<img src=\"productimages/";
                                     echo            $tmp = $row["stockNum"];
                                     echo            ".jpg\" alt=\"";
