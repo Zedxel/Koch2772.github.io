837e1ab91a11309fc48b003db16b984a30b0b8b1
Zedxel <Thomas.Temby@hotmail.com>
2020-10-23 13:58:41 +1030

    Added encryption to passwords

--- html-files/login.php
+++ html-files/login.php
@@ -13,38 +13,31 @@ $_SERVER["REQUEST_METHOD"] == "POST";
     $uname = $_POST['username'];
     $pword = $_POST['password'];
         // Prepare a select statement
-        $sql = "SELECT custid, usrnme, pwd FROM Customer WHERE usrnme = ? && pwd = ?";
-        
-        $stmt = mysqli_prepare($conn, $sql);
-            // Bind variables to the prepared statement
-            mysqli_stmt_bind_param($stmt, "ss", htmlspecialchars($uname),($pword));
-            // Attempt to execute the prepared statement
-            mysqli_stmt_execute($stmt);
-                // Store the result
-                mysqli_stmt_store_result($stmt);
-                // Check if username exists, if yes then verify password
-                $numrows = mysqli_stmt_num_rows($stmt);                  
-                    // Bind result variables   
-                    mysqli_stmt_bind_result($stmt, $cid, $usrname, $pssword);
-                    mysqli_stmt_fetch($stmt);
-                        if($numrows ==1){
-                            // Password is correct, so start a new session
-                            session_start();
-                            // Store data in session variables
-                            $_SESSION["loggedin"] = true;
-                            $_SESSION["id"] = $cid;
-                            $_SESSION["username"] = $usrname;                            
+        $sql = "SELECT * FROM `customer` WHERE `usrNme` LIKE '";
+        $sql .= $uname;
+        $sql .= "'";
+        if($result = mysqli_query($conn, $sql)){
+            if(mysqli_num_rows($result) > 0){
+                    $item = mysqli_fetch_assoc($result);
+	        }
+	    }
+        echo $item["pwd"];
+        if(password_verify($pword, trim($item["pwd"])))
+        {// Password is correct, so start a new session
+            session_start();
+            // Store data in session variables
+            header("location: index.php");
+            $_SESSION["loggedin"] = true;
+            $_SESSION["id"] = $cid;
+            $_SESSION["username"] = $usrname;                            
                             
-                            // Redirect user to the home page
-                            header("location: index.php");
-                            exit;
+            // Redirect user to the home page
+            header("location: index.php");
+            exit;
 
-                        } else { 
-                             header("location: notright.html");
-                        }
-
-            // Close statement
-            mysqli_stmt_close($stmt);
+        } else { 
+                header("location: notright.html");
+        }
 
     // Close connection
     mysqli_close($conn);
